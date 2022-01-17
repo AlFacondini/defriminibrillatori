a20af3f1c5d3762545d7b6f5b6a6917346626abe
AlFacondini <alby.faco@gmail.com>
2022-01-17 10:34:10 +0100

    Adds second api functionality (n closest defibrillators) and fixes typos

--- api/api-middleware.js
+++ api/api-middleware.js
@@ -1,3 +1,8 @@
+// api-middleware.js
+// Defines the middleware functions for the express application
+//
+// Alberto Facondini
+
 var exports = module.exports = {}
 
 const {FIREBASE_KEY, FIREBASE_URL} = require('../config/config');
@@ -8,42 +13,106 @@ var geolib = require('geolib');
 var pathToKey = path.resolve('./config', FIREBASE_KEY);
 var serviceAccount = require(pathToKey);
 
-//Login into firebase as the admin
+// Login into firebase as the admin
 admin.initializeApp({
     credential : admin.credential.cert(serviceAccount),
     databaseURL : FIREBASE_URL
 });
 
-//Move to the "defibrillatori" tree
+// Move to the "defibrillatori" tree
 var db = admin.database();
 var ref = db.ref('server/');
 var defiRef = ref.child('defibrillatori');
 
+// /closest GET middleware, responds with the closest or the n closest defibrillators
 exports.get = function(req, res, next){
     var lat = req.query.latitude;
     var lon = req.query.longitude;
     var reqCoords = [lon, lat];
-    var closest = {"address" : "ERROR",
-                    "place" : "ERROR",
-                    "coords" : [0,0],
-                    "distance" : Number.POSITIVE_INFINITY};
 
-    defiRef.once('value', 
-    (data) => {
-        data.forEach(function(node){
-            distance = geolib.getDistance(reqCoords, node.val().coords);
-            if(distance < closest.distance){
-                closest.address = node.val().address;
-                closest.coords = node.val().coords;
-                closest.place = node.val().place;
-                closest.distance = distance;
-            }
-        });
-        res.status(200).json({status : "Success", message : closest})
-    },
-    (error) => {
-        console.log(error);
-        res.status(500).json({status : "Error", message : "Error"})
-    })
+    // If n is not specified, only returns the closest
+    if(!"n" in req.query){
+        var closest = {"address" : "ERROR",
+                        "place" : "ERROR",
+                        "coords" : [0,0],
+                        "distance" : Number.POSITIVE_INFINITY};
+
+        // Iterate through the db values and find closest defib
+        defiRef.once('value', 
+        (data) => {
+            data.forEach(function(node){
+                distance = geolib.getDistance(reqCoords, node.val().coords);
+                if(distance < closest.distance){
+                    closest.address = node.val().address;
+                    closest.coords = node.val().coords;
+                    closest.place = node.val().place;
+                    closest.distance = distance;
+                }
+            });
+            sendRes(res, 200, "Success", closest);
+        },
+        (error) => {
+            console.log(error);
+            sendRes(res, 500, "ERROR", "Database Error");
+        })
+    }
+    // If n is specified, returns the n closest defibs
+    else{
+        var n = req.query.n;
+        var ret = [];
+
+        // Iterate through the db values
+        defiRef.once('value', 
+        (data) => {
+            data.forEach(function(node){
+                distance = geolib.getDistance(reqCoords, node.val().coords);
+                // If we have less than n defibs saved, just add one more
+                if(ret.length < n){
+                    ret.push({
+                        "address" : node.val().address,
+                        "place" : node.val().coords,
+                        "coords" : node.val().place,
+                        "distance" : distance
+                    })
+                    ret.sort(function(elem1, elem2){
+                        return distanceSort(elem1, elem2);
+                    });
+                }
+                // else, check if the farthest is farther away than this one and eventually exchange them
+                else{
+                    if(ret[n-1].distance > distance){
+                        ret[n-1] = {
+                            "address" : node.val().address,
+                            "place" : node.val().coords,
+                            "coords" : node.val().place,
+                            "distance" : distance
+                        };
+                        ret.sort(function(elem1, elem2){
+                            return distanceSort(elem1, elem2);
+                        });
+                    }
+                }
+            });
+            sendRes(res, 200, "Success", ret);
+        },
+        (error) => {
+            console.log(error);
+            sendRes(res, 500, "ERROR", "Database Error");
+        })
+    }
 }
 
+// Log and send the response
+function sendRes(res, httpCode, status, message){
+    console.log("Sending response.");
+    console.log("Status code:" + httpCode + " Status:" + status + " Message: " + message);
+    return res.status(httpCode).json({
+        status : status,
+        message : message
+    });
+}
+
+// Sorting function from closest to farthest node
+function distanceSort(node1, node2){
+    return node1.distance - node2.distance;
+}
--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 // api-router.js
-// Defines the routing paths for the express appication and the respective callback functions.
+// Defines the routing paths for the express application and the respective callback functions.
 //
 // Alberto Facondini
 
@@ -9,9 +9,14 @@ var middleware = require("./api-middleware");
 module.exports = function(){
     var router = express.Router();
 
+    // /closest router
+    // GET: answers with the closest or the n closest defibs 
     router.route("/closest")
         .get(middleware.get);
 
+    // /defibrillator router
+    // POST: 
+    // DELETE:   
     router.route("/defibrillator")
         .post(function(req, res){
             console.log("Connection to /defibrillator with POST from %s", req.ip);
